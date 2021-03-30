73d591d3e6a8fcad1bea462fc46ae5538554f6bd
olexandr13 <oleksandr.pelykh@gmail.com>
2021-03-30 20:12:41 +0300

    add array of issues IDs

--- action.js
+++ action.js
@@ -1,25 +1,45 @@
-const Jira = require('./common/net/Jira')
+const Jira = require("./common/net/Jira");
 
 module.exports = class {
-  constructor ({ githubEvent, argv, config }) {
+  constructor({ githubEvent, argv, config }) {
     this.Jira = new Jira({
       baseUrl: config.baseUrl,
       token: config.token,
       email: config.email,
-    })
+    });
 
-    this.config = config
-    this.argv = argv
-    this.githubEvent = githubEvent
+    this.config = config;
+    this.argv = argv;
+    this.githubEvent = githubEvent;
   }
 
-  async execute () {
-    const issueId = this.argv.issue
-    const { comment } = this.argv
+  async execute() {
+    const issueId = this.argv.issue;
+    const { comment } = this.argv;
 
-    console.log(`Adding comment to ${issueId}: \n${comment}`)
-    await this.Jira.addComment(issueId, { body: comment })
-
-    return {}
+    if (Array.isArray(issueId)) {
+      console.log(`Issues: ${issueId}`);
+      for (let i = 0; i < issueId.length; i++) {
+        issueId = makeProperIssueID(issueId);
+        console.log(`Adding comment to ${issueId}: \n${comment}`);
+        await this.Jira.addComment(issueId, { body: comment });
+      }
+    } else {
+      issueId = makeProperIssueID(issueId);
+      console.log(`Adding comment to ${issueId}: \n${comment}`);
+      await this.Jira.addComment(issueId, { body: comment });
+    }
+    return {};
   }
+};
+
+function makeProperIssueID(issueID) {
+  issueId = issueId.toUpperCase();
+  issueId.replace(" ", "-");
+  if (!issueId.includes("VTX-")) {
+    // do nothing
+  } else {
+    issueId = 'VTX-' + issueId;
+  }
+  return issueId;
 }
